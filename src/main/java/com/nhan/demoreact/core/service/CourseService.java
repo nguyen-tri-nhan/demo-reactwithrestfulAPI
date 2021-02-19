d5c94e8b24c263b3cb85115c1ec399036a1ee9ca
nguyentrinhan-dev <nguyentrinhan.dev@gmail.com>
2021-02-19 17:35:05 +0700

    delete dto

--- src/main/java/com/nhan/demoreact/core/service/CourseService.java
+++ src/main/java/com/nhan/demoreact/core/service/CourseService.java
@@ -1,8 +1,7 @@
 package com.nhan.demoreact.core.service;
 
 import com.nhan.demoreact.core.entity.Course;
-import com.nhan.demoreact.core.web.dto.CourseDto;
 
 public interface CourseService {
-    Course save(CourseDto courseDto);
+    Course save(Course courseDto);
 }
